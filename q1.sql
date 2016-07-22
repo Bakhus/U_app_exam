c48e4be7f273e5d4edc37bea11a14f110a12b37d
Bakhtiyar <bakhtiyaruz@gmail.com>
2016-07-22 10:26:36 -0400

    almost

--- q1.sql
+++ q1.sql
@@ -46,3 +46,24 @@ and DATEDIFF(hour, table1._ts, table2.request_at)<=168
 and table2.status = 'completed'
 
 group by 1,2;
+
+
+select table2.city_name, table1.day_of_week, (100*sum(completed_trip)/count(*)) as sct_percentage
+from
+(
+  select a.city_id, extract(DOW from a._ts) as day_of_week,
+         CASE WHEN DATEDIFF(hour, a._ts, b.request_at)<=168 and b.status = 'completed'
+              THEN 1
+              ELSE 0
+         END as completed_trip
+  from events as a
+  join trips as b
+  on a.city_id = b.city_id
+  where a.event_name = 'sign_up_success'
+  and a._ts BETWEEN '2016-01-01 00:00:00'::timestamp and '2016-01-08 00:00:00'::timestamp
+) as table1
+
+join cities as table2
+on table1.city_id=table2.city_id
+and table2.city_name in ('Qarth','Meereen')
+group  by  1, 2;
